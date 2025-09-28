93e6a815edaa1a754e7e43c287819e63000f45f0
jaalaw <flouz209@gmail.com>
2025-09-28 17:59:04 +0800

    top

--- src/main/java/com/slayvisual/SlayvisualClient.java
+++ src/main/java/com/slayvisual/SlayvisualClient.java
@@ -1,10 +1,55 @@
 package com.slayvisual;
 
+import com.slayvisual.config.SlayvisualConfig;
+import com.slayvisual.render.TargetVisuals;
+import com.slayvisual.ui.SlayvisualNotifications;
+import com.slayvisual.ui.screen.SlayvisualScreen;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import org.lwjgl.glfw.GLFW;
 
 public class SlayvisualClient implements ClientModInitializer {
+        private static KeyBinding openGuiKey;
+
         @Override
         public void onInitializeClient() {
+                SlayvisualNotifications.init();
+                TargetVisuals.init();
+
                 TriggerBot.init();
+
+                openGuiKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                                "key.slayvisual.gui",
+                                InputUtil.Type.KEYSYM,
+                                GLFW.GLFW_KEY_INSERT,
+                                "category.slayvisual"
+                ));
+
+                ClientTickEvents.END_CLIENT_TICK.register(client -> {
+                        while (openGuiKey.wasPressed()) {
+                                openConfigScreen(client);
+                        }
+                });
+
+                TriggerBot.setToggleKey(SlayvisualConfig.COMBAT.getTriggerBotKeyCode());
+                TriggerBot.setEnabled(SlayvisualConfig.COMBAT.isTriggerBotEnabled());
+        }
+
+        private void openConfigScreen(MinecraftClient client) {
+                if (client == null) {
+                        return;
+                }
+
+                Screen current = client.currentScreen;
+                if (current instanceof SlayvisualScreen) {
+                        client.setScreen(null);
+                } else {
+                        client.setScreen(new SlayvisualScreen());
+                }
         }
 }
---
+++ src/main/java/com/slayvisual/mixin/HeldItemRendererMixin.java
@@ -0,0 +1,79 @@
+package com.slayvisual.mixin;
+
+import com.slayvisual.config.SlayvisualConfig;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.render.VertexConsumerProvider;
+import net.minecraft.client.render.item.HeldItemRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.item.AxeItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.SwordItem;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vector3f;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(HeldItemRenderer.class)
+public abstract class HeldItemRendererMixin {
+        @Inject(method = "renderFirstPersonItem", at = @At("HEAD"))
+        private void slayvisual$applyViewModel(AbstractClientPlayerEntity player, float tickDelta, float pitch, Hand hand,
+                        float swingProgress, ItemStack stack, float equipProgress, MatrixStack matrices,
+                        VertexConsumerProvider vertexConsumers, int light, CallbackInfo ci) {
+                if (hand != Hand.MAIN_HAND) {
+                        return;
+                }
+
+                applyViewModelOffset(matrices);
+                applySwordAnimation(matrices, stack, swingProgress);
+        }
+
+        private void applyViewModelOffset(MatrixStack matrices) {
+                if (!SlayvisualConfig.VISUAL.isViewModelEnabled()) {
+                        return;
+                }
+
+                matrices.translate(SlayvisualConfig.VISUAL.getViewModelOffsetX(),
+                                SlayvisualConfig.VISUAL.getViewModelOffsetY(),
+                                SlayvisualConfig.VISUAL.getViewModelOffsetZ());
+                float scale = SlayvisualConfig.VISUAL.getViewModelScale();
+                matrices.scale(scale, scale, scale);
+        }
+
+        private void applySwordAnimation(MatrixStack matrices, ItemStack stack, float swingProgress) {
+                if (!(stack.getItem() instanceof SwordItem) && !(stack.getItem() instanceof AxeItem)) {
+                        return;
+                }
+
+                SlayvisualConfig.SwordAnimation animation = SlayvisualConfig.VISUAL.getSwordAnimation();
+                float sine = MathHelper.sin(MathHelper.sqrt(swingProgress) * (float) Math.PI);
+
+                switch (animation) {
+                        case CELESTIAL:
+                                matrices.translate(0.0f, -0.05f * sine, 0.0f);
+                                matrices.multiply(Vector3f.POSITIVE_Y.getDegreesQuaternion(8.0f * sine));
+                                break;
+                        case CASCADE:
+                                matrices.multiply(Vector3f.POSITIVE_X.getDegreesQuaternion(-18.0f * sine));
+                                matrices.multiply(Vector3f.POSITIVE_Z.getDegreesQuaternion(12.0f * sine));
+                                break;
+                        case NOVA:
+                                matrices.translate(0.02f * sine, 0.0f, 0.02f * sine);
+                                matrices.multiply(Vector3f.POSITIVE_Y.getDegreesQuaternion(-16.0f * sine));
+                                break;
+                        case VORTEX:
+                                matrices.multiply(Vector3f.POSITIVE_Y.getDegreesQuaternion(24.0f * sine));
+                                matrices.multiply(Vector3f.POSITIVE_X.getDegreesQuaternion(6.0f * sine));
+                                matrices.translate(0.0f, 0.0f, -0.04f * sine);
+                                break;
+                        case HORIZON:
+                                matrices.translate(0.0f, 0.03f * sine, -0.06f * sine);
+                                matrices.multiply(Vector3f.POSITIVE_Z.getDegreesQuaternion(-18.0f * sine));
+                                break;
+                        default:
+                                break;
+                }
+        }
+}
